b72595ff33857e581812f144620ffe73031159cb
Federico Giovannini <giovannini.federico@hotmail.it>
2021-05-25 20:47:02 +0200

    Header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,17 +21,55 @@ function Header() {
 
         <div className='hidden sm:flex items-center h-10 rounded-md flex-grow bg-yellow-400 hover:bg-yellow-500 cursor-pointer'>
           <input
-            className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none'
+            className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
             type='text'
           />
           <SearchIcon className='h-12 p-4' />
         </div>
+
+        <div className='text-white flex items-center text-sm space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hello Federico Giovannini</p>
+            <p className='font-extrabold md:text-sm'>Account & Lists</p>
+          </div>
+
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+
+          <div className='relative link flex items-center'>
+            <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+              0
+            </span>
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden font-extrabold md:text-sm md:inline mt-2'>
+              Basket
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* Bottom Nav */}
-      <div></div>
+      <div className='flex items-center bg-amazon_blue-light text-white text-sm  space-x-3 pl-6'>
+        <p className='link flex items-center'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deals</p>
+        <p className='link hidden lg:inline-flex'>Electronics</p>
+        <p className='link hidden lg:inline-flex'>Food & Grocery</p>
+        <p className='link hidden lg:inline-flex'>Prime</p>
+        <p className='link hidden lg:inline-flex'>Buy Again</p>
+        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex'>Health & Personal</p>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
+// 1:13:00
