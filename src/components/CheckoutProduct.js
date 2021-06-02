696e0904265a50fcd4d124dada17adf64b0c4131
Federico Giovannini <giovannini.federico@hotmail.it>
2021-06-02 20:45:44 +0200

    Section cashout finished

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,6 +1,8 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 
 function CheckoutProduct({
   id,
@@ -12,9 +14,31 @@ function CheckoutProduct({
   image,
   hasPrime,
 }) {
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      rating,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+    dispatch(addToBasket(product));
+  };
+
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }));
+  };
+
   return (
     <div className='grid grid-cols-5'>
       <Image src={image} height={200} width={200} objectFit='contain' />
+
+      {/* Middle */}
       <div className='col-span-3 mx-5'>
         <p>{title}</p>
         <div className='flex'>
@@ -24,7 +48,7 @@ function CheckoutProduct({
               <StarIcon className='h-5 text-yellow-500' />
             ))}
         </div>
-        <p className='text-sm my-2 line-clamp-3'>{description}</p>
+        <p className='text-xs my-2 line-clamp-3'>{description}</p>
         <Currency quantity={price} currency='GBP' />
         {hasPrime && (
           <div className='flex items-center space-x-2'>
@@ -38,13 +62,18 @@ function CheckoutProduct({
           </div>
         )}
       </div>
+
+      {/* Right */}
       <div className='flex flex-col space-y-2 my-auto justify-self-end'>
-        <button className='button mt-auto'>Add to Basket</button>
-        <button className='button mt-auto'>Remove from Basket</button>
+        <button className='button mt-auto' onClick={addItemToBasket}>
+          Add to Basket
+        </button>
+        <button className='button mt-auto' onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
       </div>
     </div>
   );
 }
 
 export default CheckoutProduct;
-
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -11,7 +11,26 @@ export const basketSlice = createSlice({
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        // The item exists in the basket
+
+        newBasket.splice(index, 1);
+      } else {
+        // The item doesn't exist
+        console.warn(
+          `Can't remove product (id: ${action.paypload.id}) as it's not in the basket`
+        );
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
@@ -19,5 +38,7 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
+export const selectTotal = (state) =>
+  state.basket.items.reduce((total, item) => total + item.price, 0);
 
 export default basketSlice.reducer;
