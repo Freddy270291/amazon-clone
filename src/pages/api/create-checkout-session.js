72b80270a4eed3a7cf9bd67cad7504e5a1fb2916
Federico Giovannini <giovannini.federico@hotmail.it>
2021-06-07 21:10:49 +0200

    Stripe checkout session

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -3,7 +3,18 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 export default async (req, res) => {
   const { items, email } = request.body;
 
-  console.log(items, email);
-};
+  const transformedItems = items.map((item) => ({
+    description: item.description,
+    quantity: 1,
+    price_data: {
+      currency: "gbp",
+      product_data: {
+        name: item.title,
+        images: [item.image],
+      },
+      unit_amount: item.price * 100,
+    },
+  }));
 
-// 41
+  const session = await stripe.checkout.sessions.create({});
+};
